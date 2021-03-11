af130b97d3e60ee827f45710378819e11d7be6e8
Thomas-Keller1 <80465083+Thomas-Keller1@users.noreply.github.com>
2021-03-11 16:43:24 +0100

    removed some testing stuff
    Removed some lines I used to test visual studio.

--- Restaurant-Groep4/Program.cs
+++ Restaurant-Groep4/Program.cs
@@ -7,24 +7,6 @@ namespace Restaurant_Groep4 {
         public static bool running = true;
         public static void Main(string[] args) {
 
-            //Console.WriteLine("Hello World!");
-            //Console.WriteLine("Not Unity cyka blyat :(");
-            //List<int> hello = new List<int>();
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    hello.Add(i);
-            //}
-            //foreach (int i in hello)
-            //{
-            //    Console.WriteLine(i);
-            //}
-            if (true) {
-                ;
-            }
-
-            int num = 5;
-            List<int> numbers = new List<int>();
-
             Start();
             
             while (running) {
