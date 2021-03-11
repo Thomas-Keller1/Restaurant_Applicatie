101aa7eb2b6f486750534343060cd1f0370cb1ed
Thomas-Keller1 <80465083+Thomas-Keller1@users.noreply.github.com>
2021-03-11 16:40:06 +0100

    Added application project
    Added a start for the application.

---
+++ Restaurant-Groep4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Restaurant_Groep4 {
+    class Program {
+
+        public static bool running = true;
+        public static void Main(string[] args) {
+
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Not Unity cyka blyat :(");
+            //List<int> hello = new List<int>();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    hello.Add(i);
+            //}
+            //foreach (int i in hello)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            if (true) {
+                ;
+            }
+
+            int num = 5;
+            List<int> numbers = new List<int>();
+
+            Start();
+            
+            while (running) {
+
+                Update();
+            }
+        }
+
+        private static void Start() {
+            ;
+        }
+
+        private static void Update() {
+            ;
+        }
+    }
+}
