69c8fc58ef6c9c5e1b9b8eb5d01d62fca69e57ae
Björn van der Velden <1007910@hr.nl>
2021-04-23 14:06:34 +0200

    Reservering datum check
    Hier word gecheckt of je een dag later de resevering nog erop zit en als dat zo is dan word die weggehaald

--- Restaurant-Groep4/Reserveringen/ReserveringHandler.cs
+++ Restaurant-Groep4/Reserveringen/ReserveringHandler.cs
@@ -13,5 +13,25 @@ namespace Restaurant_Groep4.Reserveringen
         {
             Reserveringen = new List<Reservering>();
         }
+
+        public void CheckDates(){
+
+            DateTime dateNow = DateTime.Now;
+            Date dateReservation = new Date(dateNow.Year, dateNow.Month, dateNow.Day, dateNow.Hour, dateNow.Minute);
+            int Count = 0;
+            
+            foreach(Reservering reservering in Reserveringen){
+                if (dateReservation.Day > reservering.Datum.Day){
+                    Reserveringen.RemoveAt(Count);
+                }
+                else if (dateReservation.Month > reservering.Datum.Month){
+                    Reserveringen.RemoveAt(Count);
+                }
+                Count += 1;
+            }
+            // Save the list to json file
+        }
     }
 }
+
+
