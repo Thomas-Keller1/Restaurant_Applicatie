7990bcde80c9face82f309eba6525e2ce8c0a3e6
brrrtt <80477446+brrrtt@users.noreply.github.com>
2021-06-04 14:49:28 +0200

    Accounts Saven

---
+++ Restaurant-Groep4/Inloggen/Accounts.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant_Groep4.Inloggen
+{
+    class Accounts
+    {
+    }
+}
--- Restaurant-Groep4/Inloggen/InlogHandler.cs
+++ Restaurant-Groep4/Inloggen/InlogHandler.cs
@@ -16,12 +16,46 @@ namespace Restaurant_Groep4.Inloggen
         public InlogHandler()
         {
             Account = new List<Account>();
-            // GetAccountContents();
+            GetAccountContents();
+
         }
-        public void AddAccount(Account _account)
+        public void GetAccountContents()
         {
-            Account.Add(_account);
-            //SaveAccount();
+
+            string dir = Directory.GetCurrentDirectory();
+            dir = dir.Substring(0, dir.Length - 23);
+            dir += "Json\\Accounts\\" + "Accounts" + ".json";
+            string jsonstring = File.ReadAllText(dir);
+
+            using (JsonDocument document = JsonDocument.Parse(jsonstring))
+            {
+
+                JsonElement root = document.RootElement;
+
+                if (root.TryGetProperty("Accounts", out JsonElement AccountElement))
+                {
+
+                    List<Account> TempListAccount = new List<Account>();
+
+                    foreach (JsonElement AccountsElement in AccountElement.EnumerateArray())
+                    {
+
+                        if (AccountsElement.TryGetProperty("GebruikersNaam", out JsonElement GebruikersNaamElement) && AccountsElement.TryGetProperty("Wachtwoord", out JsonElement WachtwoordElement) &&
+                           AccountsElement.TryGetProperty("Naam", out JsonElement NaamElement) && AccountsElement.TryGetProperty("Email", out JsonElement EmailElement));
+
+                    }
+                    Account = TempListAccount;
+                }
+            }
+        }
+        private void SaveAccount()
+        {
+
+            JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonstring = JsonSerializer.Serialize(this, options);
+            string dir = Directory.GetCurrentDirectory();
+            dir = dir.Substring(0, dir.Length - 23);
+            File.WriteAllText(dir + "Json\\Accounts\\" + "Accounts" + ".json", jsonstring);
         }
     }
 }
